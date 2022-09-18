e064d678e6a04509822de68b9c9e81cac4883028
Karyouchiku <107928669+Karyouchiku@users.noreply.github.com>
2022-09-19 01:27:47 +0800

    Initial Controller Testing

--- Assets/Scripts/Player Scripts/PlayerControls.cs
+++ Assets/Scripts/Player Scripts/PlayerControls.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+
+    CharacterController controller;
+    Rigidbody rb;
+    public float gravity = 5f;
+    public float jumpforce = 10f;
+    float verticalVelocity;
+    public float movementSpeed;
+    float moveX;
+    float moveZ;
+    Vector3 moveDirection;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //rb = gameObject.GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+        //movementSpeed = 3f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        moveX = Input.GetAxis("Move Horizontal") * movementSpeed;
+        moveZ = Input.GetAxis("Move Vertical") * movementSpeed;
+
+        //moveDirection = new Vector3(moveX, 0f, moveZ).normalized;
+
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -gravity * Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVelocity = jumpforce;
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+        Vector3 move = Vector3.zero;
+        move.x = moveX;
+        move.y = verticalVelocity;
+        move.z = moveZ;
+        controller.Move(move * Time.deltaTime);
+    }
+
+    void FixedUpdate() {
+        //rb.velocity = new Vector3(moveDirection.x * movementSpeed * Time.deltaTime, 0f, moveDirection.z * movementSpeed * Time.deltaTime);
         
     }
 }
