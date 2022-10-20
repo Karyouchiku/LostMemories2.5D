6021473180fc5f99577e2ba9ca02e1a599d17bea
Karyouchiku <107928669+Karyouchiku@users.noreply.github.com>
2022-10-20 21:51:08 +0800

    Loading Screen implemented

--- Assets/Scripts/ApplicationScripts/IngameMenuScript.cs
+++ Assets/Scripts/ApplicationScripts/IngameMenuScript.cs
@@ -1,26 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Threading.Tasks;
 
 public class IngameMenuScript : MonoBehaviour
 {
-    public void BackToMainMenu()
-    {
-        SceneManager.LoadScene(0);
-    }
     public void ExitGame()
     {
         Application.Quit();
         Debug.Log("Quit Game");
     }
 
+    //Delete Later start
+    //This is just for keyboard controls not for mobile
     public GameObject ingameUI;
     public Button pauseBtn;
     public Button resumeBtn;
     public Button inventoryButton;
-
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -42,5 +40,30 @@ public class IngameMenuScript : MonoBehaviour
                 inventoryButton.onClick.Invoke();
             }
         }
+
+        loadingProgress.value = Mathf.MoveTowards(loadingProgress.value, target, 3 * Time.deltaTime);
     }
+    //Delete Later End
+
+
+    public Slider loadingProgress;
+    float target;
+
+    public void LoadScene(int sceneID)
+    {
+        LoadSceneAsync(sceneID);
+    }
+
+    async void LoadSceneAsync(int sceneID)
+    {
+        var scene = SceneManager.LoadSceneAsync(sceneID);
+        scene.allowSceneActivation = false;
+        do
+        {
+            target = scene.progress;
+        } while (scene.progress < 0.9f);
+
+        scene.allowSceneActivation = true;
+    }
+    
 }
