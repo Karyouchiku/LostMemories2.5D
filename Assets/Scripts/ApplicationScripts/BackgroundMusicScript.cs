6993e3066bc8a84245932f806029bf975400712e
Karyouchiku <107928669+Karyouchiku@users.noreply.github.com>
2022-11-28 01:12:16 +0800

    Bgm Starting to apply

--- Assets/Scripts/ApplicationScripts/BackgroundMusicScript.cs
+++ Assets/Scripts/ApplicationScripts/BackgroundMusicScript.cs
@@ -7,24 +7,46 @@ public class BackgroundMusicScript : MonoBehaviour
     AudioSource bgMusic;
     [Range(0,1)]
     public float maxVolume = 0.6f;
+
+    [Range(0.1f,5f)]
+    public float fadeSpeed = 1.5f;
+    bool turn;
     void Start()
     {
-        bgMusic = gameObject.GetComponent<AudioSource>();
+        bgMusic = GetComponent<AudioSource>();
+        bgMusic.playOnAwake = true;
+        bgMusic.loop = true;
+        bgMusic.spatialBlend = 0;
     }
     // Update is called once per frame
     void Update()
     {
-        if (bgMusic.volume < maxVolume)
+        if (turn)
         {
-            bgMusic.volume += 1.5f * Time.deltaTime;
+            if (bgMusic.volume < maxVolume)
+            {
+                bgMusic.volume += fadeSpeed * Time.deltaTime;
+            }
+            if (bgMusic.volume > maxVolume)
+            {
+                bgMusic.volume -= fadeSpeed * Time.deltaTime;
+            }
         }
-        if (bgMusic.volume > maxVolume)
+        else
         {
-            bgMusic.volume -= 1.5f * Time.deltaTime;
+            if (bgMusic.volume > 0)
+            {
+                bgMusic.volume -= fadeSpeed * Time.deltaTime;
+            }
         }
+    }
+    void OnEnable()
+    {
+        turn = true;
     }
     void OnDisable()
     {
         bgMusic.volume = 0;
+        turn = false;
     }
 }
